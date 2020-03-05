268dba7d3f544451883b32a4b8508c243403de9a
aezkme <kirillctc@gmail.com>
2020-03-06 02:37:50 +0300

    fix sql schema script

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,92 +1,42 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 11.7 (Ubuntu 11.7-0ubuntu0.19.10.1)
--- Dumped by pg_dump version 11.7 (Ubuntu 11.7-0ubuntu0.19.10.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: availability; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.availability (
-    id bigint NOT NULL,
-    book_id bigint NOT NULL,
-    office_id bigint NOT NULL,
-    amount integer NOT NULL
-);
-
-
-ALTER TABLE public.availability OWNER TO postgres;
-
---
--- Name: books; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.books (
-    id bigint NOT NULL,
-    title character varying(255) NOT NULL,
-    author character varying(255) NOT NULL,
+create table books
+(
+    id          bigint                 not null,
+    title       character varying(255) not null,
+    author      character varying(255) not null,
     description text,
-    year date
+    year        date,
+    primary key (id)
 );
 
-
-ALTER TABLE public.books OWNER TO postgres;
-
---
--- Name: offices; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.offices (
-    id bigint NOT NULL,
-    name character varying(255) NOT NULL,
-    address character varying(255) NOT NULL
+create table offices
+(
+    id      bigint                 not null,
+    "name"    character varying(255) not null,
+    address character varying(255) not null,
+    primary key (id)
 );
 
+create table availability
+(
+    id        bigint  not null,
+    book_id   bigint  not null,
+    office_id bigint  not null,
+    amount    integer not null,
+    primary key (id),
+    foreign key (book_id) references books (id),
+    foreign key (office_id) references offices (id)
+);
 
-ALTER TABLE public.offices OWNER TO postgres;
+insert into books (id, title, author)
+values (0, 'first_book', 'test author'),
+       (1, 'second_book', 'test author'),
+       (2, 'third_book', 'test author');
+       (3, 'fourth_book', 'test author');
 
---
--- Data for Name: availability; Type: TABLE DATA; Schema: public; Owner: postgres
---
+insert into offices (id, name, address)
+values (0, 'first_office', 'posadskaya'),
+       (1, 'second_office', 'levashovsky');
 
-COPY public.availability (id, book_id, office_id, amount) FROM stdin;
-\.
-
-
---
--- Data for Name: books; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.books (id, title, author, description, year) FROM stdin;
-\.
-
-
---
--- Data for Name: offices; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.offices (id, name, address) FROM stdin;
-\.
-
-
---
--- PostgreSQL database dump complete
---
-
+insert into availability (id, book_id, office_id, amount)
+values (0, 0, 0, 5),
+       (1, 0, 1, 10);
