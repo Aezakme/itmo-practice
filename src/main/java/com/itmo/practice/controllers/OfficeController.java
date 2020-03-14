336fb89eeaeee374d55a34315b7530303e63c20a
annie4ka99 <47689680+annie4ka99@users.noreply.github.com>
2020-03-14 20:07:52 +0300

    Update OfficeController.java

--- src/main/java/com/itmo/practice/controllers/OfficeController.java
+++ src/main/java/com/itmo/practice/controllers/OfficeController.java
@@ -15,12 +15,13 @@ public class OfficeController {
         this.officeService = officeService;
     }
 
+    @ApiOperation(value = "Get all offices", notes = "Show all offices names")
     @GetMapping(value = "/offices")
     public String getOfficesPage() {
         return officeService.getAllOffices();
     }
 
-
+    @ApiOperation(value = "Get books by office id", notes = "Show office info books titles available in office by its id")
     @GetMapping(value = "/offices/{id}")
     public String getOfficePage(@PathVariable Long id) {
         return officeService.getOfficeInfo(id) +
